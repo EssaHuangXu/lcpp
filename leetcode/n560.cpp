cc89aba53f948d8c4d06c3ac9685045a75fa522a
EssaHuangXu <hxEssa@126.com>
2024-09-25 00:35:09 +0800

    SubarraySum

--- leetcode/n560.cpp
+++ leetcode/n560.cpp
@@ -2,19 +2,24 @@
 
 #include "n560.h"
 
+#include <unordered_map>
+
 int Solution::subarraySum(const std::vector<int>& nums, const int k) {
-  int count = 0;
-  for (auto i = 0; i < nums.size(); i++) {
-    auto target = k - nums[i];
-    for (auto j = i + 1; j < nums.size(); j++) {
-      target = target - nums[j];
-      if (target < 0) {
-        break;
-      }
-      if (target == 0) {
-        count++;
-      }
+  int result = 0;
+  std::unordered_map<int, int> map;
+  auto sum = 0;
+  map.emplace(0, 1);
+
+  // map <sum[j - 1], count>, when sum[cur] - sum[j - 1] = k  ===> sum[j -1] =
+  // sum[cur] - k
+  for (auto cur = 0; cur < nums.size(); cur++) {
+    sum += nums[cur];
+    auto target = sum - k;
+    if (map.count(target)) {
+      result += map[target];
     }
+    map[sum]++;
   }
-  return count;
+
+  return result;
 }
